f4f03f8bce1190ac09750e10c5e5b3ba78965700
mysr3809 <myasar0938@gmail.com>
2022-12-26 12:38:28 +0100

    game

---
+++ script.js
@@ -0,0 +1,15 @@
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+const cellElements = document.querySelectorAll("[data-cell]");
+let circleTurn
+
+cellElements.forEach(cell => {
+  cell.addEventListener("click", handleClick, { once: true })
+});
+
+function handleClick() {
+
+}
+
+
+
